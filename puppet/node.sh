e1e889bffce0349ac82c3e334aebdedf3601d028
Christoph Buehlmann <chrigu.b@gmail.com>
2018-01-20 13:56:09 +0100

    blade 2 hinzugefügt

--- puppet/node.sh
+++ puppet/node.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-if [ "$1" = blade1.tsbe.local ]; then
+if [ "$1" = blade1.tsbe.local ]
+then
   echo "environment: integration"
-else
+elif [ "$1" = blade2.tsbe.local ]
+then
+  echo "environment: integration"
+else 
   echo "environment: production"
 fi
