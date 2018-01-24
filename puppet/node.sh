c7373c53a00e9a5b67301fe636dfe5f83bbaab14
Christoph Buehlmann <chrigu.b@gmail.com>
2018-01-24 20:37:35 +0100

    fixed preseed

--- puppet/node.sh
+++ puppet/node.sh
@@ -4,10 +4,13 @@ then
   echo "environment: test"
 elif [ "$1" = blade2.tsbe.local ]
 then
-  echo "environment: integration"
-  elif [ "$1" = blade3.tsbe.local ]
+  echo "environment: test"
+elif [ "$1" = blade3.tsbe.local ]
 then
   echo "environment: test"
+elif [ "$1" = blade4.tsbe.local ]
+then
+  echo "environment: integration"
 else 
   echo "environment: production"
 fi
