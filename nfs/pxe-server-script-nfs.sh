a516bf7616a33bd816a1db20d3cc51c76a5959df
chbuehlmann <chrigu.b@gmail.com>
2019-12-21 10:39:21 +0100

    nfs server

---
+++ nfs/pxe-server-script-nfs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+rootcheck () {
+    if [ $(id -u) != "0" ]
+    then
+        sudo "$0" "$@"  # Modified as suggested below.
+        exit $?
+    fi
+}
+
+# install all Stuff
+rootcheck
+apt-get update -y
+apt-get upgrade -y
+apt-get install ntp nfs-kernel-server -y
+
+mkdir -p /mnt/sharefolder
+echo "/mnt/sharedfolder 192.168.1.0/24(rw,sync,no_subtree_check)
+" > /etc/exports
